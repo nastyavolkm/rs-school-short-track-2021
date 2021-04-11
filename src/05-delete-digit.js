26585b3bfb9ef01c5b6921a8f440ee3457fb5573
nastya <nastyavolkm@tut.by>
2021-04-12 02:38:42 +0300

    finish 5th

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,21 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = n.toString().split('');
+  const arrMax = [];
+  for (let i = 0; i < arr.length; i++) {
+    const arr1 = arr.slice();
+    arr1.splice(i, 1);
+    const str = arr1.join('');
+    arrMax.push(+str);
+  }
+  return arrMax.reduce((a, b) => {
+    if (a > b) {
+      return a;
+    }
+    return b;
+  });
 }
 
 module.exports = deleteDigit;
