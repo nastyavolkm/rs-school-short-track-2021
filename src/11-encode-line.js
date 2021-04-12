b977c2d85a3c0776e15179b2828f649f9765df8e
nastya <nastyavolkm@tut.by>
2021-04-13 02:04:56 +0300

    finish 11th

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const output = [];
+  let n = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      n += 1;
+    } else if (n === 1) {
+      output.push(arr[i]);
+      n = 1;
+    } else if (n > 1) {
+      output.push(`${n}${arr[i]}`);
+      n = 1;
+    }
+  }
+  return output.join('');
 }
 
 module.exports = encodeLine;
